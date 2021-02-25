b641a4099d19e7015c84f4a691e7cc50095696e4
alexandreps1123 <alexandre.ps1123@gmail.com>
2021-02-25 15:31:25 -0300

    organizada algumas linhas de codigo.

--- sessao01/exer02.c
+++ sessao01/exer02.c
@@ -1,22 +1,23 @@
-/*  Programa que pega dois numeros 'num1' e 'num2'
-    e imprime a media aritmetica deles. */
+/*
+pega dois numeros 'x' e 'y'
+depois imprime a media aritmetica deles.
+*/
 
 #include <stdio.h>
 
 int main() {
-
-    int num1, num2;
+    int x, y;
     float media;
 
-    //  Pega o num1
-    scanf("%d", &num1);
-    //  Pega o num2
-    scanf("%d", &num2);
+    /*Pega o x*/
+    scanf("%d", &x);
+    /*Pega o y*/
+    scanf("%d", &y);
 
-    //  faz a media de num1 e num2
-    media = (num1 + num2)/2.0;
+    /*faz a media de x e y, para que possa retornar um float faz-se necessario dividir por um float, nesse caso '2.0'*/
+    media = (x + y)/2.0;
 
-    //  imprime a media entre num1 e num2
+    /*imprime a media entre num1 e y*/
     printf("%.2f\n", media);
 
     return 0;
